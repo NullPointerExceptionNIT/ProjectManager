2d8d9a9888c8ce54d12cdc6fdce7c649ae064494
Vania1919 <your_email@example.com>
2025-01-10 16:29:08 +0330

    editing profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,19 @@ import Home from './Home'
 import Signup from './Signup';
 import Login from './login';
 import ProjectPage from './ProjectPage';
+import ProfilePage from './ProfilePage';
+
 function App() {
 
   return (
     <Router>
       <AuthProvider>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home/>} />
             <Route path="/Signup" element={<Signup />} />
             <Route path="/Login" element={<Login />} />
             <Route path="/ProjectPage" element={<ProjectPage />} />
+            <Route path="/ProfilePage" element={<ProfilePage />} />
           </Routes>
       </AuthProvider>
     </Router>
