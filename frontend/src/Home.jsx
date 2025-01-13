65a3103098ee61a57915ea23fd28a82edb4cd532
Vania1919 <your_email@example.com>
2025-01-13 16:56:53 +0330

    Home page fixed

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -5,35 +5,20 @@ function Home() {
   return (
     <div>
       <Header />
-      <div className="bg-white h-screen overflow-hidden">
-        <main className="flex flex-col md:flex-row">
-          <div className="md:w-1/2">
-            <h2 className="text-2xl font-semibold m-10">
-              Keep your teams running smoothly with Task Manager agent workspace
-              software
-            </h2>
-            <p className="text-gray-700 m-10">
-              Boost employee satisfaction and efficiency with time-saving
-              automations and Task Manager features that handle repetitive tasks
-              to free up agent workflows.
+      <div className="hero bg-white min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src={mainlogo}
+            className="max-w-sm rounded-lg" />
+          <div>
+            <h1 className="text-5xl font-bold">Keep your teams running smoothly with Task Manager agent workspace software!</h1>
+            <p className="py-6">
+            Boost employee satisfaction and efficiency with time-saving 
+            automations and Task Manager features that handle repetitive tasks to free up agent workflows.
             </p>
+            <button className="btn btn-primary bg-red-50">Get Started</button>
           </div>
-          <div className="md:w-1/2 flex justify-center items-center">
-            <img
-              src={mainlogo}
-              alt="Task Manager"
-              className="w-full h-auto max-w-md"
-            />
-          </div>
-          <div className="absolute mb-10 bottom-20 left-10 w-full">
-            <a
-              href="SignUp"
-              className="bg-red-300 text-black text-center px-4 py-2 rounded hover:bg-red-400 transition duration-200 w-64 inline-block"
-            >
-              Let's Start
-            </a>
-          </div>
-        </main>
+        </div>
       </div>
     </div>
   );
